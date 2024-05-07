47056d292be45599b436aaab2e6764bf2f2e6b6d
mirambialex <alexahadi069@gmail.com>
2024-05-07 10:02:55 +0100

    my-watch

---
+++ .history/clock_20240430104231.js
@@ -0,0 +1,16 @@
+<script>
+        setInterval(setClock,1000)
+        const hourHand= document.querySelector ("[data-hour-hand]")
+        const minuteHand = document.querySelector ("[data-minute-hand]")
+        const secondHand = document.querySelector ("[data-second-hand]")
+
+        function setCLASS() {
+            const currentDate=new Date()
+            const secondRatio =currentDate.getSeconds() / 60
+            const minutesRatio =(secondRatio+currentDate.getMinutes())/
+            const hoursRatio=(minutesRatio+currentDate.gethours())/
+            setRotatio(secondHand,secondsRatio)
+            setRotatio(minuteHand,minuteratio)
+            setRotatio(hourHand,hoursRatio)
+        } setrotatio
+</script>
---
+++ .history/clock_20240430104948.js
@@ -0,0 +1,19 @@
+<script>
+        setInterval(setClock,1000)
+        const hourHand= document.querySelector ("[data-hour-hand]")
+        const minuteHand = document.querySelector ("[data-minute-hand]")
+        const secondHand = document.querySelector ("[data-second-hand]")
+
+        function setCLASS() {
+            const currentDate=new Date()
+            const secondRatio =currentDate.getSeconds() / 60
+            const minutesRatio =(secondRatio+currentDate.getMinutes())/
+            const hoursRatio=(minutesRatio+currentDate.gethours())/
+            setRotatio(secondHand,secondsRatio)
+            setRotatio(minuteHand,minuteRatio)
+            setRotatio(hourHand,hoursRatio)
+        } 
+        function setRotatio(element,rotationRatio)
+        { 
+        Element.style.setProperty("")
+</script>
